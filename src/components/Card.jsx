893d837ce0b0949a6b934387218f1b79e9bd1f0b
DevOjeda016 <172947644+DevOjeda016@users.noreply.github.com>
2025-06-23 22:56:32 -0600

    add responsivity and hover to card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,16 @@
 import reactImg from '../assets/react.svg'
 const Card = () => {
   return (
-    <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-      <img src={reactImg} alt="React Logo" />
+    <div className="mx-auto flex flex-col sm:flex-row sm:items-center sm:py-4 max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10 hover:border-transparent hover:bg-sky-50">
+      <img className='size-20 sm:size-12 shrink-0' src={reactImg} alt="React Logo" />
       <div>
         <div
-          className='text-xl font-medium text-black dark:text-white'
+          className='text-xl font-medium text-black dark:text-white text-center sm:text-left'
         >
           React
         </div>
         <p
-          className='text-gray-500 dark:text-gray-400'
+          className='text-gray-500 dark:text-gray-400 text-center sm:text-left'
         >
           Now, you're is using Tailwind in React App
         </p>
